0d6ea3793a8da4278f0fa06dede843a3e3a97d56
serereg <serereg@mail.ru>
2020-12-06 02:11:03 +0300

    expense task

--- js_expense_track/script/script.js
+++ js_expense_track/script/script.js
@@ -1,31 +1,79 @@
+'use strict'
+
 window.onload = function()
 {
     document.getElementById('addExpence').addEventListener('click', addExpence);
     //debugger
-}
+};
+
+let table = {
+    head: ['Name', 'Date', 'Amount'],
+    rows: [],
+    id: 0,
+    addRow: function(data)
+    {
+        this.rows.push([this.id++, ...data]);
+    },
+    deleteRow: function(num)
+    {
+        console.log(`delete ${num}`)
+        // this.rows.forEach(element => {
+        //     if(element[0] == num)
+        //     {
+        //         element = null;
+        //     }
+        // });
+        for (let el in this.rows)
+        {
+            if(this.rows[el][0] == num)
+            {
+                console.log(this.rows[el]);
+                this.rows.splice(el, 1);
+            }
+        }
+
+    }
+};
 
 function addExpence()
 {
     let a = document.getElementById('name').value;
     let b = document.getElementById('date').value;
     let c = document.getElementById('amount').value;
-    let table = document.getElementById('ExpenseTable').getElementsByTagName('tbody')[0];
-    addRow(table, a, b, c);
+    
+    table.addRow([a, b, c]);
+    showTable(table);
+
 }
 
-function addRow(table, a, b, c)
+function showTable(table)
 {
-    let row = table.insertRow();
+    let table_html = document.getElementById('ExpenseTable').getElementsByTagName('tbody')[0];
+    table_html.innerHTML = "";
 
-    let cellA = row.insertCell();
-    cellA.append(a);
-    let cellB = row.insertCell();
-    cellB.append(b);
-    let cellC = row.insertCell();
-    cellC.append(c);
 
-    let button = document.createElement('button');
-    button.innerHTML = "Delete";
-    let cellControl = row.insertCell();
-    cellControl.append(button);
+    for (let i_r in table.rows)
+    {
+        let row = table_html.insertRow();
+        for (let i in table.rows[i_r])
+        {
+            if (i > 0)
+            {
+                let cell = row.insertCell();
+                cell.append(table.rows[i_r][i]);
+                //debugger;
+            }
+        }
+        let button = document.createElement('button');
+        button.innerHTML = "x";
+        button.setAttribute('id', table.rows[i_r][0].toString())
+        button.addEventListener('click',()=>{
+                                                table.deleteRow(table.rows[i_r][0]);
+                                                showTable(table);
+                                            });
+        let cellControl = row.insertCell();
+        cellControl.append(button);
+    }
+
 }
+
