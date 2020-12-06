09223db84d8f5cf8cb306b7128373baef565fdc1
serereg <serereg@mail.ru>
2020-12-07 00:25:33 +0300

    js note tracker. step 1

---
+++ js_note_taker/script/script.js
@@ -0,0 +1,74 @@
+'use strict'
+
+
+let modal = document.getElementById('modal');
+let button = document.getElementById('addNote');
+let span = document.getElementsByClassName('close')[0];
+
+button.onclick = function() 
+{
+    //modal.style.display = 'block';
+    let note = document.getElementById('noteText').value;
+    notes.addItem(note);
+    showTable();
+};
+
+span.onclick = function()
+{
+    modal.style.display = 'none';
+};
+
+window.onclick = function(event)
+{
+    if (event.target == modal)
+    {
+        modal.style.display = 'none';
+    }
+}
+
+let notes =
+{
+    head: [],
+    items: [],
+    id: 0,
+    addItem: function(item)
+    {
+        if(item == '') return;
+        this.items.push([this.id++, item]);
+    },
+    deleteItem: function(id)
+    {
+        for(let i in this.items)
+        {
+            if (this.items[i][0] == id)
+            {
+                this.rows.splice(i, 1);
+            }
+        }
+    }
+}
+
+function showTable()
+{
+    let table_element = document.getElementById('tableNotes').getElementsByTagName('tbody')[0];
+    table_element.innerHTML = '';
+    
+    let row = table_element.insertRow();
+
+    for (let i in notes.items)
+    {
+        if (Number(i)%2 == 0)
+        {
+            row = table_element.insertRow();
+        }
+        let cell = row.insertCell(); 
+        cell.append(createDivNote(notes.items[i][1]));
+    }
+}
+
+function createDivNote(text)
+{
+    let input = document.createElement('input');
+    input.value = text;
+    return input;
+}
